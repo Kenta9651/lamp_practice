5c403abb92faa34ea1fbddeda4c04a7b0ce25e51
Kenta Horinouchi <kh.1221.9651@gmail.com>
2020-10-14 20:01:41 +0900

    テーブル作成

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE item_history(
+    `order_id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `sum` int(11) NOT NULL,
+    `create_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `item_detail`(
+    `user_id` int(11) NOT NULL,
+    `item_id` int(11) NOT NULL,
+    `subtotal` int(11) NOT NULL,
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
